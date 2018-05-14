0823e2459f823e5a82bbbd0b9ba80f72ed8bd7b7
Patryk <mezydlo.p@gmail.com>
2018-05-15 01:14:03 +0200

    fix cpu info view
    Signed-off-by: Patryk <mezydlo.p@gmail.com>

--- system_interface.py
+++ system_interface.py
@@ -33,17 +33,36 @@ class system_interface(object):
         ret = [{"name": f, "size": s} for f, s in zip(ret_file, ret_size)]
         return ret
 
+    def get_cpu_info(self):
+        number    = []
+        usage     = []
+        freq_max  = []
+        freq_min  = []
+        freq_curr = []
+
+        freq  = psutil.cpu_freq(percpu=True)
+        cpu_usage = psutil.cpu_percent(interval=1, percpu=True)
+
+        for i in xrange(psutil.cpu_count()):
+            number.append("Cpu"+str(i))
+            freq_curr.append(str(freq[i].current)+"MHz")
+            freq_min.append(str(freq[i].min)+"MHz")
+            freq_max.append(str(freq[i].max)+"MHz")
+            usage.append(cpu_usage[i])
+
+        return [{"cpu": cpu, "usage": usage, "min": min, "max": max, "cur": cur} for cpu, usage, min, max, cur in zip(number, usage, freq_min, freq_max, freq_curr)]
+
     def get_system_information(self):
         json_info = {}
-        json_info['system']  = platform.system()
-        json_info['proc']    = platform.processor()
-        json_info['version'] = platform.version()
-        json_info['machine'] = platform.machine()
+        json_info['system']       = platform.system()
+        json_info['proc']         = platform.processor()
+        json_info['version']      = platform.version()
+        json_info['machine']      = platform.machine()
 
-        json_info['mem_percent'] = str(psutil.virtual_memory().percent)
-        json_info['mem_used']    = self.bytes_2_human_readable(psutil.virtual_memory().used)
-        json_info['mem_total']   = self.bytes_2_human_readable(psutil.virtual_memory().total)
-        json_info['mem_free']    = self.bytes_2_human_readable(psutil.virtual_memory().free)
+        json_info['mem_percent']  = str(psutil.virtual_memory().percent)
+        json_info['mem_used']     = self.bytes_2_human_readable(psutil.virtual_memory().used)
+        json_info['mem_total']    = self.bytes_2_human_readable(psutil.virtual_memory().total)
+        json_info['mem_free']     = self.bytes_2_human_readable(psutil.virtual_memory().free)
 
         json_info['disk_percent'] = str(psutil.disk_usage('/').percent)
         json_info['disk_used']    = self.bytes_2_human_readable(psutil.disk_usage('/').used)
@@ -53,26 +72,8 @@ class system_interface(object):
         json_info['uptime'] = str(datetime.timedelta(seconds=(int(time.time() - psutil.boot_time()))))
         json_info['boot_time'] = str(datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%d/%m/%Y %H:%M:%S"))
 
-        cpu_usage = psutil.cpu_percent(interval=0.1, percpu=True)
-        cpu_usage_str = ""
-        for i in xrange(len(cpu_usage)):
-            cpu_usage_str+="Cpu"+str(i)+":"+str(cpu_usage[i])+"%"+'\n'
+        json_info['cpu_info'] = self.get_cpu_info()
 
-        json_info['cpu_usage'] = cpu_usage_str
-
-        cpu_min_str = ""
-        cpu_max_str = ""
-        cpu_freq_str = ""
-
-        cpu_freq = psutil.cpu_freq(percpu=True)
-        for i in xrange(len(cpu_freq)):
-            cpu_max_str += "Cpu"+str(i)+": " + str(cpu_freq[i].max) + "MHz "
-            cpu_min_str += "Cpu"+str(i)+": " + str(cpu_freq[i].min) + "MHz "
-            cpu_freq_str += "Cpu"+str(i)+": " + str(cpu_freq[i].current) + "MHz "
-
-        json_info['cpu_freq_max'] = cpu_max_str
-        json_info['cpu_freq_min'] = cpu_min_str
-        json_info['cpu_freq_curr'] = cpu_freq_str
         return json_info
 
     def get_ping(self, ip):
