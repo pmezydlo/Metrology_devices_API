a07bd6d6726a109995c85c6dfce9e789fadc77b9
Patryk <mezydlo.p@gmail.com>
2018-07-16 17:21:24 +0200

    update install script
    Signed-off-by: Patryk <mezydlo.p@gmail.com>

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 apt-get install python3 python3-dev python3-pip
-pip3 install peewee
-pip3 install croniter
-pip3 install flask
-pip3 install psutil
+pip3 install -r requirements.txt
 wget https://github.com/python-ivi/python-vxi11/archive/master.zip
 unzip master.zip
 cd python-vxi11-master/
 python3 setup.py install
-
+mkdir files
